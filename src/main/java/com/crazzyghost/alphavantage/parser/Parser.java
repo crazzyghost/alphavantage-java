7fc5d91e3cfbd275c888d2609fa6f98c61901401
crazzyghost <ssefayeboah1@gmail.com>
2020-06-07 23:02:37 +0000

    move moshi adapter code to parser

--- src/main/java/com/crazzyghost/alphavantage/parser/Parser.java
+++ src/main/java/com/crazzyghost/alphavantage/parser/Parser.java
@@ -1,6 +1,12 @@
 package com.crazzyghost.alphavantage.parser;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.Map;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
 
 /**
  * @author crazzyghost
@@ -11,4 +17,12 @@ import java.util.Map;
 public abstract class Parser<T> {
     public abstract T onParseError(String error);
     public abstract T parse(Map<String, Object> object);
+
+    public static Map<String, Object> parseJSON(final String responseBody) throws IOException {
+        if(responseBody == null) throw new IllegalArgumentException();
+        Moshi moshi = new Moshi.Builder().build();
+        Type type = Types.newParameterizedType(Map.class, String.class, Object.class);
+        JsonAdapter<Map<String,Object>> adapter = moshi.adapter(type);
+        return adapter.fromJson(responseBody);
+    }
 }
