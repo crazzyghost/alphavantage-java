98953b8675ea8fcf8b86092e70bec31354adbb3d
crazzyghost <ssefayeboah1@gmail.com>
2021-01-25 11:45:02 +0000

    refactor

--- src/test/java/fundamentaldata/FundamentalDataRequestTest.java
+++ src/test/java/fundamentaldata/FundamentalDataRequestTest.java
@@ -14,55 +14,47 @@ import static org.junit.Assert.assertEquals;
 public class FundamentalDataRequestTest {
 
     @Test
-    public void testBalanceSheetRequest(){
+    public void testBalanceSheetRequest() {
         String expected = "https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=IBM&apikey=demo";
         FundamentalDataRequest request = new BalanceSheetRequest.Builder()
             .symbol("IBM")
             .build();
-        
         assertEquals(expected, Config.BASE_URL + UrlExtractor.extract(request) + "demo");
-            
     }
 
     @Test
-    public void testCashFlowRequest(){
+    public void testCashFlowRequest() {
         String expected = "https://www.alphavantage.co/query?function=CASH_FLOW&symbol=IBM&apikey=demo";
         FundamentalDataRequest request = new CashFlowRequest.Builder()
                 .symbol("IBM")
                 .build();
-
         assertEquals(expected, Config.BASE_URL + UrlExtractor.extract(request) + "demo");
-
     }
 
     @Test
-    public void testCompanyOverviewRequest(){
+    public void testCompanyOverviewRequest() {
         String expected = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo";
         FundamentalDataRequest request = new CompanyOverviewRequest.Builder()
                 .symbol("IBM")
                 .build();
-
         assertEquals(expected, Config.BASE_URL + UrlExtractor.extract(request) + "demo");
     }
 
     @Test
-    public void testEarningsRequest(){
+    public void testEarningsRequest() {
         String expected = "https://www.alphavantage.co/query?function=EARNINGS&symbol=IBM&apikey=demo";
         FundamentalDataRequest request = new EarningsRequest.Builder()
                 .symbol("IBM")
                 .build();
-
         assertEquals(expected, Config.BASE_URL + UrlExtractor.extract(request) + "demo");
-
     }
 
     @Test
-    public void testIncomeStatementRequest(){
+    public void testIncomeStatementRequest() {
         String expected = "https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=IBM&apikey=demo";
         FundamentalDataRequest request = new IncomeStatementRequest.Builder()
                 .symbol("IBM")
                 .build();
-
         assertEquals(expected, Config.BASE_URL + UrlExtractor.extract(request) + "demo");
     }
 }
